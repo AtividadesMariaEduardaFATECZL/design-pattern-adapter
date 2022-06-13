0e34b294009ab6a6bd7ee0eec96c94c5bfe30a6a
mariasantosdev <mariaeduardasrodolfo2@gmail.com>
2022-06-12 21:38:51 -0300

    mudando o nome da classe

--- src/main/java/br/com/maria/login/TokenLoginAdapter.java
+++ src/main/java/br/com/maria/login/TokenLoginAdapter.java
@@ -2,10 +2,10 @@ package br.com.maria.login;
 
 import br.com.maria.user.User;
 
-public class SSOLoginAdapter implements LoginAuthenticate{
+public class TokenLoginAdapter implements LoginAuthenticate{
     private final TokenLogin tokenLogin;
 
-    public SSOLoginAdapter(TokenLogin tokenLogin) {
+    public TokenLoginAdapter(TokenLogin tokenLogin) {
         this.tokenLogin = tokenLogin;
     }
 
